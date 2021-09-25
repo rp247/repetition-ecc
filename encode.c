11ac11039843920b60f6c023567ca8bbbeb383a5
rp247 <rp247@gmail.com>
2021-09-25 11:55:45 -0700

    added to do

--- encode.c
+++ encode.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+/*
+ *
+ * to do:
+ *   read in input from a stream (stdio or a file)
+ *   parse arguments (how many n repetitions, etc.)
+ *   save the encoded file
+ *   MAGIC numero
+ *   (make the encoder and decoder cross compatible)
+ *
+ */

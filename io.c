145a4b5d66b19963af4c91146ee115573c7e5baa
rp247 <rp247@gmail.com>
2021-09-26 20:05:25 -0700

    starting io

--- io.c
+++ io.c
@@ -1,1 +1,13 @@
+/*
+ * MAIN IDEA:
+ *  read n bytes from a stream 
+ *   pack n bits into a byte 
+ *   write/read from a buffer 
+ *
+ */
+
+
 #include <stdio.h>
+
+
+
---
+++ io.h
@@ -0,0 +1,18 @@
+#ifndef IO_H__
+#define IO_H__
+
+#include <stddef.h>
+
+
+/* read n bytes from a stream */
+int read_bytes(int *fd, uint64_t n, char *buf);	
+
+
+/* write n bytes from a stream */
+int write_bytes(int *fd, uint64_t n, char *buf);
+
+/* repeat the byte n times */
+int repeat_byte(char *inp, char *out, uint8_t n);
+
+
+#endif /* end header IO_H__ */

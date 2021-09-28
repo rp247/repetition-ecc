a0bddfea4eff9690975a8e31fe96949b315bf1e4
rp247 <rp247@gmail.com>
2021-09-27 22:02:14 -0700

    made all funcs. continue testing

--- io.c
+++ io.c
@@ -33,57 +33,55 @@ static uint8_t get_bit(uint8_t *buf, uint16_t ind) {
 int read_bytes(int infile, uint8_t *buf) {
 
 	int read_ret = 1;
+	uint64_t read_bytes = 0;
 
 	while ((read_ret = read(infile, buf, BUFFER)) > 0){
 		buf += read_ret;				// increment buffer index (read till BUFFER or max)
+		read_bytes += read_ret;
 	}
 
 	return read_bytes;
 }
 
 
-/*
+/* write BUFFER (4096) bytes from a buffer into a outfile */
+int wr2file(int oufile, uint8_t *buf) {
+
+	int write_ret = 1;
+	uint64_t write_bytes = 0;
+	
+	while ((write_ret = write(outfile, buf, BUFFER)) > 0 ) {
+		buf += write_ret;
+		write_bytes += write_ret;
+	}
+
+	return write_bytes;
+}
+
+
+/* read from an input buf and write the (repeated bits) to the output buf */
 int wr2buf(uint8_t *inp_buf, uint8_t *out_buf, uint8_t times) {
+
 	static uint64_t bit_index = 0;
 
 	while (bit_index < BUFFER*BYTE - times) {
 
-		for (uint8_t i = 0; i < times; i++)
-			if (get_bit(inp_buf, bit_index/BYTE)) set_bit(out_buf, bit_index);
-			else clr_bit(out_buf, bit_index);
-		}
-		
-		bit_index += times;
-	}	
+		uint8_t bit = get_bit(inp_buf, bit_index/BYTE);
 
-	if (bit_index == BUFFER) {
+		if (bit)	{ for (uint8_t i = 0; i < times; i++) { set_bit(out_buf, bit_index); } }
+		else		{ for (uint8_t i = 0; i < times; i++) { clr_bit(out_buf, bit_index); } }
+
+		bit_index += times;
+	}
+
+	// fix later
+	if (bit_index <= BUFFER || bit_index <= BUFFER-times) {
 		wr2file(outfile, out_buf);
 		bit_index = 0;
 	}
-}
-
-
-int wr2file(int outfile, uint8_t *buf) {
-	while (write(outfile, buf, BUFFER) > 0) {
-	}
-	return 0;
-}
-
-void flush_buf(int outfile) {
-	return;
-}
-*/
-
-/* repeat the individual bit n times */
-int repeat_byte(char *inp, char *out, uint8_t n) {
-	/*
-	 * read a byte from a buffer
-	 * go through each bit
-	 * write the bit to the output buffer (track output buffer index/byte)
-	 * write the buffer to output stream if full
-	 * flush the written buffer
-	 */
-
 
 }
+
+// void flush2file(int outfile, uint8_t *buf, 
+
 

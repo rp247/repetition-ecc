cfe2f260dcb7b51df651ddf3b126b57b3ad77ae2
rp247 <rp247@gmail.com>
2021-09-28 19:37:55 -0700

    continue figuring how to encode same inp and output buffer size'

--- encode.c
+++ encode.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
  *
@@ -20,7 +22,7 @@ static void usage(char *argv) {
 	fprintf(stderr,
 			        "SYNOPSIS\n"
 		       		"  A Repetition ECC encoder.\n"
-		       		"  Encodes a file using the repetition ecc code.\n"
+		       		"  Encodes a file using the repetition ecc code. (Range: 3-25)\n"
 		       		"\n"
 		       		"USAGE\n"         
 				"  ./%s [-h] [-i infile] [-o outfile]\n"         
@@ -36,10 +38,12 @@ static void usage(char *argv) {
 int main(int argc, char **argv) {
 
 	int c;
-	char *optlist = "hsi:o:";
+	char *optlist = "hsi:o:t:";
 	
 	int infile = STDIN_FILENO;	// change macro later
 	int outfile = STDOUT_FILENO;	// change macro later
+
+	uint8_t times = 3;
 
 	/* parse arguments */
 	while ((c = getopt(argc, argv, optlist)) != -1) {
@@ -64,6 +68,10 @@ int main(int argc, char **argv) {
 				}
 				break;
 
+			case 't':
+				times = (uint8_t) atoi(optarg);
+				break;
+
 			default:
 				usage(argv[0]); return -1;
 		}
@@ -78,12 +86,23 @@ int main(int argc, char **argv) {
 	 */
 
 
+	// check range
+	if (times < 3 || times > 25) {
+		fprintf(stderr, "Valid repeatition range is 3 to 25.\n");
+		return -1;
+	}
+
 	// make buffer
 	uint8_t *inp_buf = (uint8_t *) calloc(BUFFER, sizeof(uint8_t));	
 	if (!inp_buf) { fprintf(stderr, "Failed to allocate input buffer.\n"); return -1; }
 
 	// read into buffer, process the buffer, write to buf, wr2file, repeat till eof
-	//while (
+	int tot_read = 0;
+	while ((tot_read = read_bytes(infile, inp_buf)) != EOF) {
+		for (int i = 0; i < tot_read; i++) {//i <= ?	
+			wr2buf(inp_buf, out_buf, times);
+		}
+	}
 
 	// clean up
 	free(inp_buf); inp_buf = NULL;
